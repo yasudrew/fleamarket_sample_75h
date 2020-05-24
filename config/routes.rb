383a447edde822abacf98bdfeff6e25305206ee8
nagayot <nagayot0220@gmail.com>
2020-05-24 19:50:13 +0900

    Update routes.rb
    ログアウトページへのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
   get '/registration', to: 'users#new'
   get '/mypage', to: 'users#show'
 
