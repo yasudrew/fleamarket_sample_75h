20e3cb09bc5a5753e8ce3fc8cbcfe8f0d847c54a
forest0720 <forest0720@users.noreply.github.com>
2020-05-31 19:53:41 +0900

    出品登録ver2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,6 +7,7 @@ class ItemsController < ApplicationController
     @item.build_shipping
     @item.build_brand
     @item.images.build
+    @item.build_category
     render layout: 'sub_application'
   end
 
@@ -15,11 +16,14 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.create(item_params)
-    if @item.save!
+    fee = item_params[:price].to_i * 0.1
+    profit = item_params[:price].to_i - fee
+    @item = Item.new(item_params.merge(fee: fee, profit: profit))
+    if @item.save
       redirect_to root_path
    else
       redirect_to new_item_path
+      #flash.now[:alert] = ‘登録に失敗しました。お手数ですが、もう一度やり直してください。’
    end
   end
 
@@ -28,7 +32,7 @@ class ItemsController < ApplicationController
   end
   private
   def item_params
-    params.require(:item).permit(:name,:description,:status,:price,:fee,:profit,:buyer_id,
+    params.require(:item).permit(:name,:description,:status,:price,:buyer_id,
     category_attributes: [:id,:name], brand_attributes: [:id ,:name], user_id: [:nickname, :email, :password],shipping_attributes:[:id,:burden, :method, :area, :day],
     images_attributes:[:id,:image])
     .merge(user_id: current_user.id)
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,12 +5,28 @@ class Item < ApplicationRecord
   belongs_to :shipping
   has_many :images, dependent: :destroy
   has_many :comments, dependent: :destroy
+
+  accepts_nested_attributes_for :category
   accepts_nested_attributes_for :shipping
   accepts_nested_attributes_for :brand
   accepts_nested_attributes_for :images
+
 
   validates :images,presence: true
   validates :name,length:{maximum:20}
   validates :description, length:{maximum:1000}
   validates :price,numericality:{greater_than_or_equal_to: 300} 
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :status
+    validates :price
+    validates :fee
+    validates :profit
+    validates :user_id
+    # validates :category_id 未解決問題　保存できなくなる
+    # validates :shipping_id　未解決問題　保存できなくなる
+    validates :images
+  end
 end
