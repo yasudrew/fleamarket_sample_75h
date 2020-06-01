c4fe3ff7ecdb2dafd91b22d9986a219fbccd0e04
yasudrew <63987719+yasudrew@users.noreply.github.com>
2020-06-01 13:01:38 +0900

    set_card一時外し

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_024709) do
+ActiveRecord::Schema.define(version: 2020_05_31_044106) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2020_05_28_024709) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "ancestry"
+    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
