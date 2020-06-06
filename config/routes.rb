7e88a6e03d20b2274ea895d6179abd77d90ecf52
nagayot <nagayot0220@gmail.com>
2020-06-06 16:52:10 +0900

    勘違いで消したものを作り直し

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/logout_page'
+  get 'session/logout_page'
   devise_for :users, controllers: {
     omniauth_callbacks: 'users/omniauth_callbacks',
     registrations: 'users/registrations'
--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get sessions_new_url
+  test "should get logout_page" do
+    get sessions_logout_page_url
     assert_response :success
   end
-
 end
