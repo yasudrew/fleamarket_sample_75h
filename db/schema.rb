8e5a4163bdec78dcd2a6ec0e717877767758d694
yasudrew <63987719+yasudrew@users.noreply.github.com>
2020-06-08 13:16:44 +0900

    コメント機能修正

--- db/schema.rb
+++ db/schema.rb
@@ -81,15 +81,15 @@ ActiveRecord::Schema.define(version: 2020_06_03_142537) do
   end
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "family_name", null: false
-    t.string "first_name", null: false
-    t.string "family_name_kana", null: false
-    t.string "first_name_kana", null: false
+    t.string "family_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "family_name_kana", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
     t.date "birth_day", null: false
-    t.string "post_code", null: false
-    t.string "prefecture", null: false
-    t.string "city", null: false
-    t.string "address", null: false
+    t.string "post_code", default: "", null: false
+    t.string "prefecture", default: "", null: false
+    t.string "city", default: "", null: false
+    t.string "address", default: "", null: false
     t.string "building_name"
     t.integer "user_id", null: false
     t.datetime "created_at", null: false
